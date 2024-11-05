be1ba6462e9f405b2eb22c53a991ea9b64712861
nurgal1ev <nurgaliev0644@gmail.com>
2024-11-05 11:57:10 +0300

    footer input

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 customSelect('select');
+
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label = document.querySelector('.subscribe__label');
+
+inputSubscribe.addEventListener('input', ()=> {
+    if (!inputSubscribe.value.trim()) {
+        label.classList.add('.subscribe__label--top');
+    } else {
+        label.classList.remove('.subscribe__label--top');
+    }
+});
